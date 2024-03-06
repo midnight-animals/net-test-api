97ae97591ce1a164ad19be14d760ed2a65841522
JaclyNolan <anhbg330011@gmail.com>
2024-03-06 22:23:41 +0700

    Add .env package and conceal secret

--- Program.cs
+++ Program.cs
@@ -3,7 +3,6 @@ using net_test_api.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.Configure<MongoDBSettings>(builder.Configuration.GetSection("MongoDB"));
 builder.Services.AddSingleton<MongoDBService>();
 
 // Add services to the container.
--- Services/MongoDBService.cs
+++ Services/MongoDBService.cs
@@ -5,26 +5,32 @@ using MongoDB.Bson;
 
 namespace net_test_api.Services;
 
-public class MongoDBService {
+public class MongoDBService
+{
     private readonly IMongoCollection<Post> _postCollection;
 
-    public MongoDBService(IOptions<MongoDBSettings> mongoDBSettings) {
-        MongoClient client = new MongoClient(mongoDBSettings.Value.ConnectionURI);
-        IMongoDatabase database = client.GetDatabase(mongoDBSettings.Value.DatabaseName);
-        _postCollection = database.GetCollection<Post>(mongoDBSettings.Value.CollectionName);
+    public MongoDBService()
+    {
+        DotNetEnv.Env.Load();
+        MongoClient client = new MongoClient(Environment.GetEnvironmentVariable("CONNECTION_URI"));
+        IMongoDatabase database = client.GetDatabase(Environment.GetEnvironmentVariable("DATABASE_NAME"));
+        _postCollection = database.GetCollection<Post>(Environment.GetEnvironmentVariable("COLLECTION_NAME"));
     }
 
-    public async Task CreateAsync(Post post) {
+    public async Task CreateAsync(Post post)
+    {
         post.createdAt = DateTime.Now;
         await _postCollection.InsertOneAsync(post);
         return;
     }
 
-    public async Task<List<Post>> GetPostsAsync() {
+    public async Task<List<Post>> GetPostsAsync()
+    {
         return await _postCollection.Find(new BsonDocument()).ToListAsync();
     }
 
-    public async Task UpdatePostAsync(string id, Post post) {
+    public async Task UpdatePostAsync(string id, Post post)
+    {
         FilterDefinition<Post> filter = Builders<Post>.Filter.Eq("Id", id);
         UpdateDefinition<Post> update = Builders<Post>.Update
         .Set("tilte", post.title)
@@ -36,7 +42,8 @@ public class MongoDBService {
         return;
     }
 
-    public async Task DeletePostAsync(string id) {
+    public async Task DeletePostAsync(string id)
+    {
         FilterDefinition<Post> filter = Builders<Post>.Filter.Eq("Id", id);
         await _postCollection.DeleteOneAsync(filter);
         return;
